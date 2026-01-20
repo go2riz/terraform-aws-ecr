bb3b371b56b7b7e1a410f9e1a5be4eea6c31e577
Haider <go2riz@gmail.com>
2026-01-20 08:57:38 +0500

    Refactor code

--- moved.tf
+++ moved.tf
@@ -1,16 +1,14 @@
-terraform {
-  moved {
-    from = aws_ecr_repository.default
-    to   = aws_ecr_repository.this[0]
-  }
+moved {
+  from = aws_ecr_repository.default
+  to   = aws_ecr_repository.this[0]
+}
 
-  moved {
-    from = aws_ecr_lifecycle_policy.default
-    to   = aws_ecr_lifecycle_policy.this[0]
-  }
+moved {
+  from = aws_ecr_lifecycle_policy.default
+  to   = aws_ecr_lifecycle_policy.this[0]
+}
 
-  moved {
-    from = aws_ecr_repository_policy.default
-    to   = aws_ecr_repository_policy.this[0]
-  }
+moved {
+  from = aws_ecr_repository_policy.default
+  to   = aws_ecr_repository_policy.this[0]
 }
